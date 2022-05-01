02d857fa81df1c158b3e649baa42123a413ad1c6
Theophilus E.K Adukpo <theoadu@gmail.com>
2022-05-01 13:10:04 +0000

    initial setup commit

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import "./navbar.css";
+import Notification from "../../img/notification-bell-svgrepo-com.svg";
+import Settings from "../../img/settings.svg";
+import Message from "../../img/message.svg";
+
+const Navbar = ({ socket }) => {
+  const [notifications, setNotifications] = useState([]);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    socket.on("getNotification", (data) => {
+      setNotifications((prev) => [...prev, data]);
+    });
+  }, [socket]);
+
+  const displayNotification = ({ senderName, type }) => {
+    let action;
+
+    let i = 0;
+    if (type === 1) {
+      action = "liked";
+    } else if (type === 2) {
+      action = "commented";
+    } else {
+      action = "shared";
+    }
+    return (
+      <span
+        className="notification"
+        key={`${senderName} ${i++}` }
+      >{`${senderName} ${action} your post`}</span>
+    );
+  };
+
+  const handleRead = () => {
+    setNotifications([]);
+    setOpen(false);
+  };
+
+  return (
+    <div className="navbar">
+      <span className="logo">TEKA App</span>
+      <div className="icons">
+        <div className="icon" onClick={() => setOpen(!open)}>
+          <img src={Notification} className="iconImg" alt="" />
+          {notifications.length > 0 && (
+            <div className="counter">{notifications.length}</div>
+          )}
+        </div>
+        <div className="icon" onClick={() => setOpen(!open)}>
+          <img src={Message} className="iconImg" alt="" />
+        </div>
+        <div className="icon" onClick={() => setOpen(!open)}>
+          <img src={Settings} className="iconImg" alt="" />
+        </div>
+      </div>
+      {open && (
+        <div className="notifications">
+          {notifications.map((n) => displayNotification(n))}
+          <button className="nButton" onClick={handleRead}>
+            Mark as read
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
